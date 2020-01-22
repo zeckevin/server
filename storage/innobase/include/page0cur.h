fcf24c7352a8af7543e5667dbcd9743b028e1c17
Marko Mäkelä <marko.makela@mariadb.com>
2020-01-22 18:51:29 +0200

    MDEV-12353: Replace MLOG_*_END_COPY_CREATED
    Instead of writing the high-level redo log records
    MLOG_LIST_END_COPY_CREATED, MLOG_COMP_LIST_END_COPY_CREATED
    write log for each individual insert of a record.
    
    page_copy_rec_list_end_to_created_page(): Remove.
    
    This will improve the fill factor of some pages.
    Adjust some tests accordingly.
    
    PageBulk::init(), PageBulk::finish(): Avoid setting bogus limits
    to PAGE_HEAP_TOP and PAGE_N_DIR_SLOTS. Avoid accessor functions
    that would enforce these limits before the correct ones are set
    at the end of PageBulk::finish().

--- storage/innobase/include/page0cur.h
+++ storage/innobase/include/page0cur.h
@@ -1,7 +1,7 @@
 /*****************************************************************************
 
 Copyright (c) 1994, 2016, Oracle and/or its affiliates. All Rights Reserved.
-Copyright (c) 2018, 2019, MariaDB Corporation.
+Copyright (c) 2018, 2020, MariaDB Corporation.
 
 This program is free software; you can redistribute it and/or modify it under
 the terms of the GNU General Public License as published by the Free Software
@@ -217,6 +217,7 @@ IMPORTANT: The caller will have to update IBUF_BITMAP_FREE
 if this is a compressed leaf page in a secondary index.
 This has to be done either within the same mini-transaction,
 or by invoking ibuf_reset_free_bits() before mtr_commit(). */
+ATTRIBUTE_COLD /* only used when crash-upgrading */
 void
 page_copy_rec_list_end_to_created_page(
 /*===================================*/
